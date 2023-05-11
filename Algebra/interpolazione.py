83cac65b6c18ad5d5957a65bd88549762ddcec40
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-11 21:27:19 +0200

    definita interpolazione e plot dei graifci

--- Algebra/interpolazione.py
+++ Algebra/interpolazione.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot
 from scipy import *
 from numpy import *
 
@@ -14,4 +15,47 @@ def Vandermond(x):
 
 
 
-def lagrange(x,y)
+def lagrange(x,y,xx):
+    """
+    INPUT
+    x: vettore dei nodi
+    y: vettore delle ordinate
+    xx: vettore di ascisse in cui valutare pn(x)
+
+    OUTPUT
+    yy: vettore delle valutazioni di pn(x) nelle ascisse xx(i)
+    """
+    n=shape(x)[0]  #numero di nodi
+    yy=0
+    for k in range(n):
+        Lk=1
+        for i in range(n):
+            if i!=k: Lk*=((xx-x[i])/(x[k]-x[i]))  #xx è un vettore. Quindi a ogni elemento di xx sottraggo x[i] e ottengo
+                                                  # un vettore. Poi CON *
+                                                  # STO FACENDO IL PRODOTTO TRA GLI ELEMENTI DELLO STESSO INDICE
+        yy+=(y[k]*Lk)
+    return yy
+
+def interpola(f,int,n):
+    """
+    INPUT
+    f : funzione
+    int: intervallo di interpolazione
+    n: numero di nodi equidistanti che ricoprono l'intervallo int
+    """
+    x=linspace(int[0],int[1],n) #vettore dei nodi
+    y=f(x) #vettore delle ordinate
+    xx=linspace(int[0],int[1],100) #vettore di ascisse in cui valutare pn(x)
+    yy=lagrange(x,y,xx) #vettore delle valutazioni di pn(x) nelle ascisse xx(i)
+    fxx=f(xx) #valutazione della funzione f nelle ascisse xx(i)
+    pyplot.plot(x,y,'bo',xx,fxx,'red',xx,yy,'green')
+    pyplot.grid()
+    pyplot.title="INTERPOLAZIONE DI LAGRANGE"
+    pyplot.xlabel="ASSE DELLE X"
+    pyplot.ylabel="ASSE DELLE Y"
+    pyplot.show()
+
+def f(x):
+    return e**(-x)*sin(x)
+
+interpola(f,[0,pi],5)
